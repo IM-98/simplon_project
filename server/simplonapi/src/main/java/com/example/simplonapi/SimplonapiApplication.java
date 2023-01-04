f1c5a19f5531b330df84dc8bfb3e095b498e736f
IM-98 <108485107+IM-98@users.noreply.github.com>
2023-01-04 13:41:45 +0100

    first commit

---
+++ server/simplonapi/src/main/java/com/example/simplonapi/SimplonapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplonapiApplication.class, args);
+	}
+
+}
---
+++ server/simplonapi/src/test/java/com/example/simplonapi/SimplonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
