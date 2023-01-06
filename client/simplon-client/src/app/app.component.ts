ac68ba5464818b5aa068f2e9410a8b3a717466fb
Damien LAPORTA <dadicy85@gmail.com>
2023-01-06 13:22:43 +0100

    header v2 ok pour seb

--- client/simplon-client/src/app/app.component.ts
+++ client/simplon-client/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filrouge';
+
+  showButton: boolean = true;
+
+  hideButton() {
+    this.showButton = false;
+  }
+
 }
