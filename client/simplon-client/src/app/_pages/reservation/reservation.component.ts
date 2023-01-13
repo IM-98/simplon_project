32c20e3a4c2a132681aa15486ade6a3f0ba7fef7
sebastienlestrat <sebastien.le-strat@hotmail.fr>
2023-01-13 14:25:47 +0100

    Resa

--- client/simplon-client/src/app/_pages/reservation/reservation.component.ts
+++ client/simplon-client/src/app/_pages/reservation/reservation.component.ts
@@ -11,7 +11,57 @@ import { ReservationService } from 'src/app/services/reservation.service';
   styleUrls: ['./reservation.component.scss']
 })
 export class ReservationComponent implements OnInit {
+
+  rezaForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
   ngOnInit(): void {
-      
+    this.rezaForm = this.formBuilder.group ({
+        nom: ['', [
+          Validators.required,
+        ]],
+        email: ['', [
+          Validators.required,
+          Validators.email
+        ]],
+        nbAdults: ['', [
+          Validators.required,
+          Validators.minLength(1)
+        ]],
+        dateReservation: ['', [
+          Validators.required,
+
+        ]],
+        cartes: ['', [
+          Validators.required,
+        ]]
+
+      });
+}
+      get nom() {
+        return this.rezaForm.get('nom') as FormControl;
+      }
+      get email() {
+        return this.rezaForm.get('email') as FormControl;
+      }
+      get nbAdults() {
+        return this.rezaForm.get('nbAdults') as FormControl;
+      }
+      get dateReservation() {
+        return this.rezaForm.get('dateReservation') as FormControl;
+      }
+      get cartes() {
+        return this.rezaForm.get('cartes') as FormControl;
+      }
+
+  onSubmit() {
+    if (this.rezaForm!.valid) {
+
+      alert('Formulaire valide !');
+    } else {
+      alert('Formulaire invalide !');
+    }
   }
 }
+
